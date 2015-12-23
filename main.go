3eab1587e5d3a6bc78b0f1868fd5f4d4354d7568
Richard Todd <waywardcode@users.noreply.github.com>
2015-12-23 14:22:16 -0600

    ran go fmt on the source

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
-        "strconv"
-        "strings"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -17,21 +17,21 @@ const (
 	end_of_program = 0 // indicates there are no more lines
 	end_of_line    = 0 // indicates the end of a line
 
-        lnumber_prefix   = 0x0E  // line number prefix, unsigned
+	lnumber_prefix = 0x0E // line number prefix, unsigned
 
-        octal_prefix  = 0x0B  // comes before an octal constant, signed
-        hex_prefix    = 0x0C  // comes before a hex constant, signed
-        first_ten     = 0x11    // 0x11 through 0x1B are the constants 0 to 10
-        last_ten     = 0x1B    // 0x11 through 0x1B are the constants 0 to 10
-        int_prefix_1b   = 0x0F   // one-byte int prefix, 11 - 255
-        int_prefix_2b   = 0x1C   // two-byte int prefix, signed
+	octal_prefix  = 0x0B // comes before an octal constant, signed
+	hex_prefix    = 0x0C // comes before a hex constant, signed
+	first_ten     = 0x11 // 0x11 through 0x1B are the constants 0 to 10
+	last_ten      = 0x1B // 0x11 through 0x1B are the constants 0 to 10
+	int_prefix_1b = 0x0F // one-byte int prefix, 11 - 255
+	int_prefix_2b = 0x1C // two-byte int prefix, signed
 
-        float_prefix_4b   = 0x1D   // four-byte int prefix 
-        float_prefix_8b   = 0x1F   // eight-byte float prefix
- 
-        unused_const = 0x10   // if we see these tokens, it's an error
-        unused_const2 = 0x1E  // if we see these tokens, it's an error
-        lpointer_prefix  = 0x0D  // line pointer, we should NEVER see this
+	float_prefix_4b = 0x1D // four-byte int prefix
+	float_prefix_8b = 0x1F // eight-byte float prefix
+
+	unused_const    = 0x10 // if we see these tokens, it's an error
+	unused_const2   = 0x1E // if we see these tokens, it's an error
+	lpointer_prefix = 0x0D // line pointer, we should NEVER see this
 )
 
 type byteDripper interface {
@@ -78,78 +78,77 @@ func read_lineno(in byteDripper) (uint16, error) {
 	return read_uint16(in)
 }
 
-
 func lookup_token(tok byte) string {
-   return fmt.Sprintf("<TOKEN %02x>",tok)
+	return fmt.Sprintf("<TOKEN %02x>", tok)
 }
 
 func get_next_tokstr(in byteDripper) (ans string) {
-   	tok, _ := in.ReadByte()
- 
-        // it might represent itself
-        if tok >= 0x20 && tok <= 0x7E {
-           ans = string(tok)
-           return
-        }
+	tok, _ := in.ReadByte()
 
-        // it might be a hard-coded low number
-        if tok >= first_ten && tok <= last_ten {
-           ans = strconv.Itoa( int(tok) - first_ten )
-           return
-        }
+	// it might represent itself
+	if tok >= 0x20 && tok <= 0x7E {
+		ans = string(tok)
+		return
+	}
 
-        // it might require more bytes read
-      	switch tok  {
-           case lnumber_prefix, octal_prefix, hex_prefix, int_prefix_2b:
-                in.ReadByte()
-                in.ReadByte()
-                ans = "<NUMBER>" 
-           case int_prefix_1b:
-                in.ReadByte()
-                ans = "<NUMBER>" 
-           case float_prefix_4b: 
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                ans = "<FNUMBER_4>" 
-           case float_prefix_8b: 
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                in.ReadByte()
-                ans = "<FNUMBER_8>" 
-           case end_of_line:
-                // do nothing
-           default:
-               ans = lookup_token(tok)
-        }
+	// it might be a hard-coded low number
+	if tok >= first_ten && tok <= last_ten {
+		ans = strconv.Itoa(int(tok) - first_ten)
+		return
+	}
 
-        return 
+	// it might require more bytes read
+	switch tok {
+	case lnumber_prefix, octal_prefix, hex_prefix, int_prefix_2b:
+		in.ReadByte()
+		in.ReadByte()
+		ans = "<NUMBER>"
+	case int_prefix_1b:
+		in.ReadByte()
+		ans = "<NUMBER>"
+	case float_prefix_4b:
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		ans = "<FNUMBER_4>"
+	case float_prefix_8b:
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		in.ReadByte()
+		ans = "<FNUMBER_8>"
+	case end_of_line:
+		// do nothing
+	default:
+		ans = lookup_token(tok)
+	}
+
+	return
 }
 
 func cat(in byteDripper) {
 	for {
 		line, err := read_lineno(in)
 		if err != nil {
-                        fmt.Printf("\nERROR %s\n",err.Error())
- 			break
+			fmt.Printf("\nERROR %s\n", err.Error())
+			break
 		}
 
-		var tok = strconv.FormatUint(uint64(line),10)
-                var toks = []string{tok}
-                tok = "  "
-                for tok != "" {
-                    toks = append(toks,tok)
-                    tok = get_next_tokstr(in)
-                }
+		var tok = strconv.FormatUint(uint64(line), 10)
+		var toks = []string{tok}
+		tok = "  "
+		for tok != "" {
+			toks = append(toks, tok)
+			tok = get_next_tokstr(in)
+		}
 
-                fmt.Printf("%s\n", strings.Join(toks,""))
-                
+		fmt.Printf("%s\n", strings.Join(toks, ""))
+
 	}
 }
 
