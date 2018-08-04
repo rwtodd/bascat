b972445de2e423e7c7d7673da6635372d360e701
Richard Todd <rwtodd@users.noreply.github.com>
2018-08-04 15:56:33 -0500

    Add project files.

---
+++ BasCat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
