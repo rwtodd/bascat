4b132e49cca6a7bf6471b2195ed891aede7a372c
Richard Todd <rwtodd@users.noreply.github.com>
2018-08-06 21:58:05 -0500

    added usage string

--- BasCat/Program.cs
+++ BasCat/Program.cs
@@ -8,6 +8,12 @@ namespace BasCat
     {
         static void Main(string[] args)
         {
+            if(args.Length != 1)
+            {
+                Console.Error.WriteLine("USAGE: bascat <gw-basic file>");
+                Environment.Exit(-1);
+            }
+
             new BasCat(File.ReadAllBytes(args[0])).PrintAllLines(Console.Out);            
         }
     }
