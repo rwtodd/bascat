b1df8dba3a205734929ceccee8605dcc4c0c2a15
Richard Todd <rwtodd@users.noreply.github.com>
2019-01-26 17:36:21 -0600

    Moved test script for win32 bascat
    Preparing for addition of UWP version

--- run-tests.ps1
+++
@@ -1,6 +0,0 @@
-# Run the debug binary, and then compare the output
-# to known good output.
-# The run of fc.exe should leave $LASTEXITCODE set appropriately.
-
-& "${PSSCRIPTROOT}\x64\Debug\BasCat.exe" "${PSSCRIPTROOT}\Tests\NEPTUNE.gwbas" > "$env:TEMP\bctest.txt"
-& fc.exe "${PSSCRIPTROOT}\Tests\NEPTUNE.txt"  "$env:TEMP\bctest.txt"
