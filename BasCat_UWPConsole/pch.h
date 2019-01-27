e67f3b8ab5ec94d537887b04842c0183d25ccd34
Richard Todd <rwtodd@users.noreply.github.com>
2019-01-26 18:05:16 -0600

    Added BasCat UWP console app
    It can only read files from the current directory or below, but that's
    acceptable for an app like this.

---
+++ BasCat_UWPConsole/pch.h
@@ -0,0 +1,9 @@
+﻿// pch.h
+// Header for standard system include files.
+
+#pragma once
+
+#include "winrt/Windows.ApplicationModel.Core.h"
+#include "winrt/Windows.Storage.h"
+#include "winrt/Windows.Storage.Streams.h"
+#include<Windows.h>
