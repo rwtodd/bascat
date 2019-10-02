dfdaed08c1d07def6883172d6668d0ae70c0892a
Richard Todd <rwtodd@users.noreply.github.com>
2019-10-01 21:54:30 -0500

    Slight adjustment to MBF32/64 decoders (similar to my Go version).

--- src/main/java/org/rwtodd/bascat/MBFReader.java
+++ src/main/java/org/rwtodd/bascat/MBFReader.java
@@ -1,6 +1,7 @@
 package org.rwtodd.bascat;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.MathContext;
 import java.nio.ByteBuffer;
 
@@ -17,12 +18,14 @@ abstract class MBFReader {
             return 0.0;
         } 
         final var sign = new BigDecimal((0x0080_0000 & fourBytes) == 0 ? 1 : -1, MathContext.DECIMAL128);
-        final var exp = ((fourBytes >> 24) & 0xff) - 129;
-        final var TWO = new BigDecimal(2, MathContext.DECIMAL128);
-        final var expt = (exp < 0) ? BigDecimal.ONE.divide(TWO.pow(-exp, MathContext.DECIMAL128), MathContext.DECIMAL128) : TWO.pow(exp, MathContext.DECIMAL128);
-        final var scand = new BigDecimal(fourBytes & 0x007f_ffff, MathContext.DECIMAL128);
-        return sign.multiply(
-                scand.divide(new BigDecimal(0x80_0000L, MathContext.DECIMAL128)).add(BigDecimal.ONE)).multiply(expt).doubleValue();
+        final int exp = ((fourBytes >> 24) & 0xff) - 129;
+        final var scaled = new BigDecimal(BigInteger.ONE.shiftLeft(Math.abs(exp)), MathContext.DECIMAL128);
+        final var expt = (exp < 0) ? BigDecimal.ONE.divide(scaled, MathContext.DECIMAL128) : scaled;
+        final var scand = new BigDecimal((fourBytes & 0x007f_ffff)|0x0080_0000, MathContext.DECIMAL128);
+        return scand.divide(new BigDecimal(0x80_0000L, MathContext.DECIMAL128))
+                .multiply(sign, MathContext.DECIMAL128)
+                .multiply(expt, MathContext.DECIMAL128)
+                .doubleValue();
     }
 
     public static double getMBF64(final ByteBuffer bb) {
@@ -33,11 +36,13 @@ abstract class MBFReader {
 
         final var sign = new BigDecimal((0x00800000_00000000L & eightBytes) == 0 ? 1 : -1, MathContext.DECIMAL128);
         final int exp = (int)((eightBytes >> 56) & 0xff) - 129;
-        final var TWO = new BigDecimal(2, MathContext.DECIMAL128);
-        final var expt = (exp < 0) ? BigDecimal.ONE.divide(TWO.pow(-exp, MathContext.DECIMAL128), MathContext.DECIMAL128) : TWO.pow(exp, MathContext.DECIMAL128);
-        var scand = new BigDecimal(eightBytes & 0x007fffff_ffffffffL, MathContext.DECIMAL128);
-        return sign.multiply(
-                scand.divide(new BigDecimal(0x80_0000_0000_0000L, MathContext.DECIMAL128)).add(BigDecimal.ONE)).multiply(expt, MathContext.DECIMAL128).doubleValue();
+        final var scaled = new BigDecimal(BigInteger.ONE.shiftLeft(Math.abs(exp)), MathContext.DECIMAL128);
+        final var expt = (exp < 0) ? BigDecimal.ONE.divide(scaled, MathContext.DECIMAL128) : scaled;
+        var scand = new BigDecimal((eightBytes & 0x007fffff_ffffffffL)|0x00800000_00000000L, MathContext.DECIMAL128);
+        return scand.divide(new BigDecimal(0x80_0000_0000_0000L, MathContext.DECIMAL128))
+                .multiply(sign, MathContext.DECIMAL128)
+                .multiply(expt, MathContext.DECIMAL128)
+                .doubleValue();
     }
 
 }
